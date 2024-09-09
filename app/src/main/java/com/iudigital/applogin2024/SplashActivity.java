3a70361e8a26a4a6a6705167df438606751d660a
Bayron <bayronmg99@gmail.com>
2024-09-09 13:15:17 -0500

    Version 1 App Alta pinta *Splash *Login *Registro

---
+++ app/src/main/java/com/iudigital/applogin2024/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.iudigital.applogin2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        //Agregar animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+
+        ImageView logoAlta = findViewById(R.id.logoAlta);
+        logoAlta.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        } ,4000);
+    }
+}
